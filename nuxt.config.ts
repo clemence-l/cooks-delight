1da950e93f332c2c811e9bb91df7982ebbe4812a
clemence-l <clemence.lauga@hotmail.com>
2025-12-02 14:29:47 +0100

    feat(app) : init nuxt app

---
+++ nuxt.config.ts
@@ -0,0 +1,37 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  css: ["~/styles/main.scss"],
+  components: [
+    {
+      path: "~/components",
+      pathPrefix: false,
+      extensions: ["vue"],
+    },
+  ],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            @use "@/styles/foundations/functions" as *;
+            @use "@/styles/foundations/variables" as *;
+            @use "@/styles/foundations/mixins" as *;
+          `,
+        },
+      },
+    },
+  },
+
+  runtimeConfig: {
+    public: {
+      apiUrl: "",
+    },
+  },
+  modules: [
+    "@nuxt/eslint",
+    "@nuxt/image",
+    "@nuxt/test-utils",
+  ],
+});
