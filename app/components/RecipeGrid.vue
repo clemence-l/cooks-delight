ef8236d279748eff569d804a080a4f9d56b3f288
clemence-l <clemence.lauga@hotmail.com>
2025-12-03 09:37:41 +0100

    feat(recipe) : add grid recipe component

--- app/components/RecipeGrid.vue
+++ app/components/RecipeGrid.vue
@@ -13,3 +13,40 @@ const [{ data: recipes, error }] = await Promise.all([
 
 if (error && error.value) throw new Error("Page not Found");
 </script>
+
+<template>
+  <ul class="recipes">
+    <li v-for="(recipe, index) in recipes" :key="index">
+      <RecipeCard>
+        <template #img>
+          <NuxtImg :src="recipe.image_url" />
+        </template>
+        <template #h3>{{ recipe.title }}</template>
+        <template #desc>{{ recipe.description }}</template>
+        <template #ratings>
+          Ratings
+          <!-- {{ recipe.rating }} -->
+          <!-- <img src="/img/star.svg" alt="" /> -->
+        </template>
+        <template #link>
+          <NuxtLink :to="`/recipe/${recipe.recipe_id}`">
+            <MyButton variant="outline">View Recipe</MyButton>
+          </NuxtLink>
+        </template>
+      </RecipeCard>
+    </li>
+  </ul>
+</template>
+
+<style lang="scss">
+.pages {
+  display: flex;
+  flex-direction: column;
+}
+.recipes {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: rem(16);
+  grid-auto-rows: minmax(100px, auto);
+}
+</style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,6 @@
+<!-- eslint-disable vue/no-multiple-template-root -->
 <template>
   <MyText variant="h1">Hello !</MyText>
+
+  <RecipeGrid />
 </template>
