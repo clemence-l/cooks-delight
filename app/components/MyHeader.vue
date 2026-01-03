e1af73e79656184b9bbd1874961d232c0dee20f2
clemence-l <clemence.lauga@hotmail.com>
2026-01-03 13:35:29 +0100

    feat(sanity) : add header with sanity site settings

--- app/components/MyHeader.vue
+++ app/components/MyHeader.vue
@@ -1,36 +1,41 @@
+<script setup lang="ts">
+import type { SanityImageSource } from "@sanity/image-url";
+import type { SanitySiteSettings } from "~/types/cms/site-settings";
+
+defineProps<{
+  logo: SanityImageSource;
+  navigation: SanitySiteSettings["navigation"];
+}>();
+
+const { urlFor } = useSanityImage();
+const token = useCookie("recipe_token");
+</script>
+
 <template>
   <header>
-    <MyButton href="/index.html" variant="empty">
-      <!-- <img src="/img/logo.svg" alt="" /> -->
-      <MyText variant="tag">logo</MyText>
-    </MyButton>
+    <NuxtLink to="/">
+      <MyButton variant="empty">
+        <img :src="urlFor(logo)?.url()" alt="Logo Cooks Delight" >
+      </MyButton>
+    </NuxtLink>
 
     <nav>
       <ul>
-        <li>
-          <MyButton href="/" variant="link" state="active">Recettes</MyButton>
-        </li>
-        <li>
-          <MyButton href="/" variant="link">À Propos</MyButton>
+        <li v-for="item in navigation" :key="item.label">
+          <NuxtLink :to="item.url">
+            <MyButton variant="link">{{ item.label }}</MyButton>
+          </NuxtLink>
         </li>
       </ul>
     </nav>
     <div>
-      <MyButton variant="grey" content="icon">
-        <svg
-          width="16"
-          height="16"
-          viewBox="0 0 16 16"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M12.6267 11.5129L16 14.8861L14.8861 16L11.5129 12.6267C10.3 13.5971 8.76177 14.1776 7.08881 14.1776C3.17579 14.1776 0 11.0018 0 7.08881C0 3.17579 3.17579 0 7.08881 0C11.0018 0 14.1776 3.17579 14.1776 7.08881C14.1776 8.76177 13.5971 10.3 12.6267 11.5129ZM11.0465 10.9284C12.0096 9.93584 12.6023 8.58187 12.6023 7.08881C12.6023 4.04259 10.135 1.57529 7.08881 1.57529C4.04259 1.57529 1.57529 4.04259 1.57529 7.08881C1.57529 10.135 4.04259 12.6023 7.08881 12.6023C8.58187 12.6023 9.93584 12.0096 10.9284 11.0465L11.0465 10.9284Z"
-            fill="#262522"
-          />
-        </svg>
-      </MyButton>
-      <MyButton variant="default">S'inscrire</MyButton>
+      <NuxtLink v-if="token" to="/dashboard">
+        <MyButton variant="default">Votre profil</MyButton>
+      </NuxtLink>
+
+      <NuxtLink v-else to="/login">
+        <MyButton variant="default">Connexion</MyButton>
+      </NuxtLink>
     </div>
   </header>
 </template>
@@ -43,7 +48,7 @@ header {
   margin-bottom: rem(16);
   display: flex;
   justify-content: space-between;
-  padding: rem(16) rem(24);
+  padding: rem(8) rem(24);
   border: 1px solid var(--dark-24);
   border-radius: rem(32);
   align-items: center;
